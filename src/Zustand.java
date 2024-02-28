fd02f48322e2cffc3ddbe95de783a8d8c4ddb62b
Maxi361 <gurkexd07@gamil.com>
2024-02-28 15:27:15 +0100

    1

---
+++ src/Zustand.java
@@ -0,0 +1,16 @@
+public abstract class Zustand {
+  
+  public Zustand() {
+  }
+
+  public List<Zustand> gibFolgezustaende0() {
+     return null;
+  }
+
+  public List<Zustand> gibFolgezustaende1() {
+     return null;
+  }
+
+  public abstract boolean istEndzustand();
+
+}
